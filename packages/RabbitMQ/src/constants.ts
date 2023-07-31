b3bf7639ee7b4abedc4763bca1ad0fab608c1f8b
Jorge Clavijo <jym272@gmail.com>
2023-07-31 15:43:07 -0300

    feat: comment in rabbit package (#2)

--- packages/RabbitMQ/src/constants.ts
+++ packages/RabbitMQ/src/constants.ts
@@ -1,4 +1,4 @@
 export const REQUEUE_EXCHANGE = 'requeue_exchange';
 export const NACKING_DELAY_MS = 2000;
-
+// some comment here to test the behavior of the snippet
 export const MAX_NACK_RETRIES = 10;
